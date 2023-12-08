6ed5c642cc02b76bf648c1893b94d1111198b385
Euny Kim <euny4535@gmail.com>
2023-12-08 11:10:17 +0900

    - 레벨 4 완료

--- app/src/main/java/com/example/mgckiosk/action/Basket.kt
+++ app/src/main/java/com/example/mgckiosk/action/Basket.kt
@@ -1,9 +1,10 @@
 package com.example.mgckiosk.action
 
+import com.example.mgckiosk.obj.OrderList
+import com.example.mgckiosk.obj.OrderList.dataList
 
+// OrderList.orderList.add(menuItem)
 open class Basket() {
-    val balance = 10
-    var dataList = mutableListOf<MutableMap<String, MutableList<Double>>>() //
     fun saveData(
         list: List<Triple<String, Double, String>>,
         menu: Int,
@@ -13,32 +14,35 @@ open class Basket() {
         var singlePrice = list[menu].second
         var menuUnit = unit
         // var data = listOf(menuName: String, menuUnit: Int, menuPrice: Double)
-        if (dataList != emptyList<MutableMap<String, MutableList<Double>>>()) {
 
-            for (i in dataList.indices) {
-                var subDataList = dataList[i][menuName]
-                subDataList?.set(0, subDataList.get(0) + menuUnit)
-                subDataList?.set(1, subDataList.get(0) * singlePrice)
+        if (dataList == emptyList<MutableMap<String, MutableList<Double>>>()) {
+            dataList.add(mutableMapOf("Menu" to mutableListOf(0.0, 0.0)))
+        }
+
+        if (dataList[0].keys.contains(menuName)) {
+            var subList = dataList[0][menuName]?.toMutableList()
+            subList!!.set(0, subList!!.get(0) + menuUnit)
+            subList!!.set(1, subList!!.get(0) * singlePrice)
+            dataList[0].put(menuName, subList)
+        } else {
+            dataList[0].put(menuName, mutableListOf(unit.toDouble(), singlePrice*unit))
+        }
+
+        println("[ 저장된 데이터 목록 ]")
+        var keyList = OrderList.dataList[0].keys.toMutableList()
+        for (i in 1 until keyList.size) {
+            var subList = OrderList.dataList[0][keyList[i]]
+            println("${i}. 메뉴: ${keyList[i]}, 수량: ${subList?.get(0)}, 이 제품 총 가격: ${subList?.get(1)}")
+        }
+        for (i in 1 until OrderList.dataList.size) {
+            var keyList = OrderList.dataList[0].keys.toMutableList()
+            for (j in 1 until keyList.size) {
+                var valueList = OrderList.dataList[0][keyList[j]]
+                println("${i}. 제품 이룸: ${keyList[j]}, 수량: ${valueList?.get(0)?.toInt()}, 이 제품 총 가격: ${valueList?.get(1)}")
             }
-        } else {
-            var subData = mutableListOf(menuUnit.toDouble(), singlePrice * menuUnit)
-            var data = mutableMapOf(menuName to subData)
-            dataList.add(data)
         }
     }
 
-    fun checkBalance() {
-        var totalPrice:Double = 0.0
-        for (i in dataList.indices) {
-            var subDataList = dataList[i].values
-            var unitPriceList = subDataList.take(0)[0][1]
-            totalPrice += unitPriceList
-        }
-        if (totalPrice > balance) {
-            println("현재 잔액은 ${(balance*1000).toInt()}원 으로 ${(totalPrice-balance)*1000.toInt()}원이 부족해서 주문할 수 없습니다.")
-        } else {
 
-        }
-    }
 
 }
